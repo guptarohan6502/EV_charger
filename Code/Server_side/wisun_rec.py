1b7d81ab6f14c522ad9aaf64c08136b33e7faf91
ROHAN GUPTA <82900156+guptarohan6502@users.noreply.github.com>
2025-04-05 19:17:29 +0530

    Update wisun_rec.py

--- Code/Server_side/wisun_rec.py
+++ Code/Server_side/wisun_rec.py
@@ -1,11 +1,10 @@
-
 import socket
 import wisun_comms
 import Onem_evcharger
 
 HOST_IP = "fd12:3456::1"
 
-UDP_send_IP = "fd12:3456::5232:5fff:fe42:615b"
+UDP_send_IP = "fd12:3456::5232:5fff:fe42:8b86"
 
 UDP_send_PORT = 5001
 UDP_rec_PORT = 5005
@@ -16,20 +15,23 @@ sock_recv.bind((HOST_IP, UDP_rec_PORT))
 sock_send = socket.socket(socket.AF_INET6,  # Internet
                           socket.SOCK_DGRAM)  # UDP
 
-valid_id = ["216950627631"]
-
+print("Script is running")
 while True:
     data, addr = sock_recv.recvfrom(1024)  # buffer size is 1024 bytes
     print("received message: %s" % data)
     data = data.decode('utf-8')
+    
+    if("Emergency" in data):
+        print(data)
+    else:
+        # Example: "{'Amount': 99, 'VehicleidTag': '216950627631', 'Time': 1620000000, 'Chargerid': 'EV-L001-03'}"
+        txn_response = Onem_evcharger.verify_transaction(eval(data))
 
-    txn_response = Onem_evcharger.verify_transaction(eval(data))
-
-    if("idTag" in str(data)):
-        if(txn_response == "Transaction Approved"):
+    if "idTag" in str(data):
+        if txn_response == "Transaction Approved":
             wisun_comms.send_toserver(
                 "valid_yes", sock_send, UDP_send_IP, UDP_send_PORT)
-        elif(txn_response == "Insufficient Balance"):
+        elif txn_response == "Insufficient Balance":
             wisun_comms.send_toserver(
                 "valid_insuff", sock_send, UDP_send_IP, UDP_send_PORT)
         else:
